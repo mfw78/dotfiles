880c73311fc0856039782781f4021992e1d19a48
mfw78 <mfw78@nullis.xyz>
2025-01-15 10:05:02 +0000

    chore(nvim): remove rustacean and configure nvchad rust_analyzer

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -14,26 +14,6 @@ return {
   },
 
   {
-    'mrcjkb/rustaceanvim',
-    version = '^5', -- Recommended
-    lazy = false, -- This plugin is already lazy
-    config = function ()
-      local mason_registry = require('mason-registry')
-      local codelldb = mason_registry.get_package("codelldb")
-      local extension_path = codelldb:get_install_path() .. "/extension/"
-      local codelldb_path = extension_path .. "adapter/codelldb"
-      local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
-      local cfg = require('rustaceanvim.config')
-
-      vim.g.rustaceanvim = {
-        dap = {
-          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-        },
-      }
-    end
-  },
-
-  {
     'mfussenegger/nvim-dap',
     config = function ()
       local dap, dapui = require("dap"), require("dapui")
